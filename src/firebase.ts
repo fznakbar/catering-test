af2508591881ba80804a1392377f5bf5907de4f9
fznakbar <fauzanakbar10@gmail.com>
2022-07-23 15:02:48 +0700

    add readme and setup firebase hosting

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1K9dDQFdUglKZXhreI2AHLK7jzrhMmyk",
+  authDomain: "alami-test.firebaseapp.com",
+  projectId: "alami-test",
+  storageBucket: "alami-test.appspot.com",
+  messagingSenderId: "388699447005",
+  appId: "1:388699447005:web:d9bbb1fde3c871f5570534",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
